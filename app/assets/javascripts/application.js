5f0deadbd0a297d5d20f6c86a9780451e5735be4
finisher1017@gmail.com <finisher1017@gmail.com>
2015-04-21 20:57:34 -0700

    second dynamic select

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,15 +18,21 @@
 var laptop_models = ["Asset Model", "Latitude E7240",
                      "Latitude E6510", "Latitude E6420"];
 
-var printer_models = ["Asset Model", "Latitude E7240",
-                     "Latitude E6510", "Latitude E6420"];
+var printer_models = ["Asset Model", "Xerox",
+                     "Canon", "Brother", "HP", "Epson"];
 
 $(document).ready( function() {
 	$('#asset_type').change( function() {
 		if ($('#asset_type').val() == 'laptops') {
 			$('#dell_desktop_model').find('option').remove();
-			laptop_models.forEach(function(model) {
-				$('#dell_desktop_model').append('<option value="'+model+'">'+model+'</option>');
+			laptop_models.forEach(function(lapmodel) {
+				$('#dell_desktop_model').append('<option value="'+lapmodel+'">'+lapmodel+'</option>');
+			})
+		}
+		if ($('#asset_type').val() == 'printers') {
+			$('#dell_desktop_model').find('option').remove();
+			printer_models.forEach(function(printmodel) {
+				$('#dell_desktop_model').append('<option value="'+printmodel+'">'+printmodel+'</option>');
 			})
 		}
 		$.get($('#asset_type').val(), function(data) {
